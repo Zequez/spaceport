bd00cf53f2e4e770f0f6cc9270425c968dac1f70
Zequez <zequez@gmail.com>
2023-06-26 19:40:40 -0300

    Added letters and optimized mobile layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import type { CollectionEntry } from "astro:content";
 import Header from "./Header";
+import { useRef, useEffect, useState } from "react";
 import { useSites } from "../lib/useSites";
 import SiteCard from "./SiteCard";
 
@@ -7,24 +8,56 @@ type Site = CollectionEntry<"sites">;
 
 export default function App({ sites }: { sites: Site[] }) {
   const s = useSites(sites);
+  const scrollerRef = useRef<HTMLDivElement>(null);
+  const scrollLettersSpace = useRef<HTMLDivElement>(null);
+  // const [letterHeight, setLetterHeight] = useState();
+
+  // useEffect(() => {
+  //   scr
+  // }, [])
+
+  function handleScroll() {
+    console.log("Scrolling");
+  }
+
+  const lettersEntries = Array.from(s.byLetter);
 
   return (
-    <div className="relative h-screen w-screen bg-slate-600 overflow-y-scroll text-white">
-      <nav className="fixed z-10 inset-y-0 right-0 w-12 sm:w-20 bg-black/30"></nav>
-      {/* <div className="relative h-screen overflow-hidden pr-12 sm:pr-20">
+    <div
+      className="relative bg-slate-600 text-white"
+      // ref={scrollerRef}
+      // onScroll={handleScroll}
+    >
+      <nav className="fixed z-10 inset-y-0 right-0 w-12 sm:w-20 bg-black/30 flex flex-col pb-2">
+        <a href="#">
+          <img src={s.imageBySlug("spaceport")} className="w-12 h-12 sm:w-20 sm:h-20" />
+        </a>
+        <div className="overflow-hidden flex-grow flex flex-col items-center">
+          {lettersEntries.map(([letter, _]) => (
+            <a
+              href={`#${letter}`}
+              key={letter}
+              style={{ height: `${100 / lettersEntries.length}%` }}
+            >
+              {letter}
+            </a>
+          ))}
+        </div>
+      </nav>
+      <div className="relative h-screen overflow-hidden pr-12 sm:pr-20">
         <div className="absolute inset-0 z-0 pointer-events-none overflow-hidden bg-right bg-cover bg-[url(/assets/bg.avif)]"></div>
         <Header />
-      </div> */}
+      </div>
       <div className="pr-12 sm:pr-20">
-        {Array.from(s.byLetter).map(([letter, sites]) => (
-          <div className="p-4" key={letter}>
+        {lettersEntries.map(([letter, sites]) => (
+          <div className="p-2 sm:p-4" key={letter} id={letter}>
             <h2
               className="text-5xl mb-4 text-center font-black"
               style={{ textShadow: "1px 1px 2px #000" }}
             >
               {letter}
             </h2>
-            <div className="grid grid-cols-1 gap-4 max-w-screen-lg mx-auto ">
+            <div className="grid grid-cols-1 gap-2 sm:gap-4 max-w-screen-lg mx-auto ">
               {sites.map((site) => (
                 <SiteCard key={site.slug} s={s} site={site} />
               ))}
--- src/components/SiteCard.tsx
+++ src/components/SiteCard.tsx
@@ -4,61 +4,91 @@ export default function SiteCard({ site, s }: { site: Site; s: SitesUtils }) {
   return (
     <div
       key={site.id}
-      className="border bg-white/90 overflow-hidden text-black/80 border-white/20 rounded-md flex shadow-md"
+      className="border bg-white/90 overflow-hidden text-black/80 border-white/20 rounded-md shadow-md"
     >
-      <a href={site.data.url} className="flex-shrink-0">
+      <div className="sm:flex sm:flex-row">
+        {thumbnail()}
+        <div className="contents sm:block">
+          {title()}
+          {replacedBy()}
+          {categories()}
+          <div className="clear-both sm:clear-none"></div>
+          {description()}
+          {alsoSee()}
+        </div>
+      </div>
+    </div>
+  );
+
+  function thumbnail() {
+    return (
+      <a href={site.data.url} className="flex-shrink-0 float-left mr-2 sm:mr-0 sm:float-none">
         <img
-          className="h-40 w-40"
+          className="h-16 w-16 sm:h-40 sm:w-40"
           src={site.data.replacedBy ? "/assets/moved.webp" : s.imageBySlug(site.slug)}
           loading="lazy"
         />
       </a>
-      <div>
-        <h3
-          className="text-3xl font-bold tracking-wider px-2 py-1 text-gray-600"
-          style={{ textShadow: "0 1px #fff" }}
-        >
-          {site.data.title}
-        </h3>
-        {site.data.replacedBy ? (
-          <div className="px-2 mb-4 markdown">
-            <strong>NOTE:</strong> The website has been moved to{" "}
-            {s.linkifySlug(site.data.replacedBy)}
-          </div>
-        ) : null}
-        {site.data.categories.length ? (
-          <div className="px-2 mb-4">
-            {site.data.categories.map((cat) => (
-              <span key={cat}>{cat}</span>
-            ))}
-          </div>
-        ) : null}
-        {site.body ? (
-          <div
-            className="mx-2 p-2 rounded-md shadow-inner border border-gray-300 mb-2 markdown bg-white/50"
-            dangerouslySetInnerHTML={{ __html: s.linkifySlugsInText(site.body) }}
-          ></div>
-        ) : null}
-        {site.data.alsoSee.length ? (
-          <div className="px-2">
-            <div className="font-bold tracking-wider text-gray-600 mb-2">See also</div>
-            <div className="flex flex-wrap">
-              {site.data.alsoSee.map((otherSite) => (
-                <div
-                  key={typeof otherSite === "string" ? otherSite : otherSite.text}
-                  className="bg-black/10 rounded-sm overflow-hidden border border-gray-400 mb-2 mr-2"
-                >
-                  {typeof otherSite === "string" ? (
-                    s.linkifySlug(otherSite)
-                  ) : (
-                    <a href={otherSite.url}>{otherSite.text}</a>
-                  )}
-                </div>
-              ))}
-            </div>
-          </div>
-        ) : null}
+    );
+  }
+
+  function title() {
+    return (
+      <h3
+        className="text-xl sm:text-3xl font-bold tracking-wider px-2 py-1 text-gray-600"
+        style={{ textShadow: "0 1px #fff" }}
+      >
+        {site.data.title}
+      </h3>
+    );
+  }
+
+  function replacedBy() {
+    return site.data.replacedBy ? (
+      <div className="px-2 mb-4 markdown">
+        <strong>NOTE:</strong> The website has been moved to {s.linkifySlug(site.data.replacedBy)}
       </div>
-    </div>
-  );
+    ) : null;
+  }
+
+  function categories() {
+    return site.data.categories.length ? (
+      <div className="px-2 mb-4">
+        {site.data.categories.map((cat) => (
+          <span key={cat}>{cat}</span>
+        ))}
+      </div>
+    ) : null;
+  }
+
+  function description() {
+    return site.body ? (
+      <div
+        className="mx-2 p-2 mt-2 rounded-md shadow-inner border border-gray-300 mb-2 markdown bg-white/50"
+        dangerouslySetInnerHTML={{ __html: s.linkifySlugsInText(site.body) }}
+      ></div>
+    ) : null;
+  }
+
+  function alsoSee() {
+    return site.data.alsoSee.length ? (
+      <div className="px-2">
+        <div className="font-bold tracking-wider text-gray-600 mb-2">See also</div>
+        <div className="flex flex-wrap">
+          {site.data.alsoSee.map((otherSite) => (
+            <div
+              key={typeof otherSite === "string" ? otherSite : otherSite.text}
+              className="bg-black/10 rounded-sm overflow-hidden border border-gray-400 mb-2 mr-2"
+            >
+              {typeof otherSite === "string" ? (
+                s.linkifySlug(otherSite)
+              ) : (
+                <a href={otherSite.url}>{otherSite.text}</a>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    ) : null;
+  }
 }
